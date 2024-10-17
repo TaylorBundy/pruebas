58238dc4b80aef04b522ee2284c52f2882f03b25
Taylor Bundy <martonbarbosa@hotmail.com>
2024-10-17 19:41:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".MySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    breakpoints: {
+        991: {
+            slidesPerView: 4
+        }
+    }
+});
+
+const slides = document.querySelectorAll('.swiper-slide');
+let index = 0;
+
+function prevSlide(){
+    slides[index].classList.remove('active');
+    index--;
+
+    if(index < 0)
+        index = slides.length -1;
+
+    slides[index].classList.add('active');      
+}
+
+document.querySelector('.prev').addEventListener('click', e => {
+    prevSlide();
+});
+
+function nextSlide(){
+    slides[index].classList.remove('active');
+    index++;
+
+    if(index > slides.length -1)
+        index = 0;
+
+    slides[index].classList.add('active');      
+}
+
+document.querySelector('.next').addEventListener('click', e => {
+    nextSlide();
+});
